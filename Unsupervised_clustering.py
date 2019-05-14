6251e1f8e4463edcd507587ee57ba1cba6685611
rohithramesh1991 <49020018+rohithramesh1991@users.noreply.github.com>
2019-05-15 00:31:48 +0530

    Add files via upload

---
+++ Unsupervised_clustering.py
@@ -0,0 +1,39 @@
+﻿#Text pre-processing
+"""removes punctuation, stopwords, and returns a list of the remaining words, or tokens"""
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+#Cleaning the text
+
+import string
+def text_process(text):
+    '''
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Return the cleaned text as a list of words
+    4. Remove words
+    '''
+    stemmer = WordNetLemmatizer()
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join([i for i in nopunc if not i.isdigit()])
+    nopunc =  [word.lower() for word in nopunc.split() if word not in stopwords.words('english')]
+    return [stemmer.lemmatize(word) for word in nopunc]
+
+#Vectorisation : -
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidfconvert = TfidfVectorizer(analyzer=text_process,ngram_range=(1,3)).fit(X_train)
+
+X_transformed=tfidfconvert.transform(X_train)
+
+# Clustering the training sentences with K-means technique
+
+from sklearn.cluster import KMeans
+modelkmeans = KMeans(n_clusters=60, init='k-means++', n_init=100)
+modelkmeans.fit(X_transformed)
+
