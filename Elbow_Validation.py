b020de6ced2f1b9b70cc793aa48e2682798baba5
rohithramesh1991 <49020018+rohithramesh1991@users.noreply.github.com>
2019-05-15 19:35:42 +0530

    Add files via upload

---
+++ Elbow_Validation.py
@@ -0,0 +1,19 @@
+﻿checking for optimal number of clusters
+
+from sklearn.cluster import KMeans
+
+Sum_of_squared_distances = []
+K = range(1,150)
+for k in K:
+    km = KMeans(n_clusters=k)
+    km = km.fit(X_transformed)
+    Sum_of_squared_distances.append(km.inertia_)
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(K, Sum_of_squared_distances, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Sum_of_squared_distances')
+plt.title('Elbow Method For Optimal k')
+plt.show()
